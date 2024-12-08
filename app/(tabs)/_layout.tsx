14eea6bb9b0b2389c6d6fca842dc54e7f403d7fa
yn5 <ynsnetmail@gmail.com>
2024-12-08 15:32:49 +0100

    cleanup code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,7 +22,6 @@ export default function TabLayout() {
           tabBarBackground: TabBarBackground,
           tabBarStyle: Platform.select({
             ios: {
-              // Use a transparent background on iOS to show the blur effect
               position: 'absolute',
             },
             default: {},
@@ -40,13 +39,6 @@ export default function TabLayout() {
           options={{
             title: 'New',
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.circle.fill" color={color} />,
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: 'Explore',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
           }}
         />
       </Tabs>
--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -7,12 +7,7 @@ import { OpaqueColorValue, StyleProp, TextStyle, ViewStyle } from 'react-native'
 
 // Add your SFSymbol to MaterialIcons mappings here.
 const MAPPING = {
-  // See MaterialIcons here: https://icons.expo.fyi
-  // See SF Symbols in the SF Symbols app on Mac.
   'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
   'plus.circle.fill': 'add-circle',
 } as Partial<
   Record<
